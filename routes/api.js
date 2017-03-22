19813410ade57acc32bffc9c6bc1d2c81a66b84d
Maxwell Sands <maxsands1503@icloud.com>
2017-03-22 17:59:23 -0600

    CRUD complete, will add more advanced routes in the future

--- routes/api.js
+++ routes/api.js
@@ -13,11 +13,57 @@ router.get('/', function(req,res){
 router.get('/Pages', function(req, res){
   return Page.find(function(err, pages){
     if(!err){
-      return response.send(pages)
+      return res.send(pages)
     }else{
-      return response.send(500, err)
+      return res.send(500, err)
     }
   });
 });
+
+router.post('/pages/add', function(req, res){
+  var page = new Page({
+    title: req.body.title,
+    url: req.body.url,
+    content: req.body.content,
+    menuIndex: req.body.menuIndex,
+    date: new Date(Date.now())
+  });
+
+  page.save(function(err){
+    if(!err){
+      return res.send(200, page);
+    }else{
+      res.send(500, err);
+    }
+  });
+});
+
+router.post('/pages/update', function(req, res){
+  var id = req.body._id;
+
+  Page.update({
+    _id: id
+  }, {
+    $set:{
+      title: req.body.title,
+      url: req.body.url,
+      content: req.body.content,
+      menuIndex: req.body.menuIndex,
+      date: new Date(Date.now())
+    }
+  }).exec();
+  res.send("Page Updated");
+});
+router.get('/pages/delete/:id', function(req, res){
+  var id = req.params.id;
+  Pages.remove({
+    _id:id
+  },
+    function(err){
+      return console.log(err);
+    });
+    return res.send('Page id- ' + id + ' has been deleted.')
+});
+
 
 module.exports = router;
